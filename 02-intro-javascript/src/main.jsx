72f16853db9eea67d1c4d277e249fdc542af80ce
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-05-07 19:49:55 -0500

    new commit

---
+++ 02-intro-javascript/src/main.jsx
@@ -0,0 +1,14 @@
+
+const persona = {
+    nombre: "Rafael",
+    apellido: "Chuco",
+    edad: 18,
+    direccion:{
+        ciudad: "lima",
+        zip: 1234354123,
+        lat: 3523523,
+        lng: 4124332,
+    }
+}
+
+console.log(persona);
